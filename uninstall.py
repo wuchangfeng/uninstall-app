56538e5441b93a3b9333b815fb3304ca1ff579e1
allen <437806668@qq.com>
2016-07-12 08:58:01 +0800

    Update uninstall.py
    添加清除 LogCat 缓存的功能

--- uninstall.py
+++ uninstall.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-
 import os;
 
 # 删除所有你指定包名的 APP
 def delAllapp( ):
-    
     print 'start delete all your app in your Phone or Simulator '
     os.popen('adb wait-for-device');
     corename = raw_input("input your app package corename:")
     oriPackages = os.popen('adb shell pm list packages {name}'.format(name=corename));
-
+    # list all PackageName
     for oriPackage in oriPackages:
-        
         deletePackage = oriPackage.split(':')[1]
         os.popen('adb uninstall ' + deletePackage );
         print deletePackage + "is deleted"
@@ -20,14 +17,12 @@ def delAllapp( ):
 
 # 删除所有你指定包名的特定 APP
 def listAllpackage( ):
-    
     i = 0
     os.popen('adb wait-for-device');
     corename = raw_input("input your app package corename:")
     oriPackages = os.popen('adb shell pm list packages {name}'.format(name=corename));
 
     for oriPackage in oriPackages:
-        
         deletePackage = oriPackage.split(':')[1]
         print str(i) + ":" + deletePackage
         deleteList.append(deletePackage)
@@ -36,15 +31,20 @@ def listAllpackage( ):
 def deleteApp(number):
     os.popen('adb uninstall ' + deleteList[number] );
     print 'delete '+ deleteList[number] + "success"
+ 
+# 清除 LogCat 缓存   
+def clearLogcat( ):
+    print 'start clear logcat buffer in your Phone or Simulator'
+    os.popen('adb wait-for-device');
+    os.popen('adb logcat -c');
+    print 'logcat is cleared success'       
     
 
 if __name__ == '__main__':
     
     delAllapp()
-    ```
-    another fun here
-    ```
     #deleteList = []
     #listAllpackage()
     #number = raw_input("input the number of app you want to delete:")
     #deleteApp(int(number))
+    clearLogcat()
